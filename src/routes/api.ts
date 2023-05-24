f4f4aab79082fd7cf232dee48a260ed9bc516674
Dennis Na <4e65rv@gmail.com>
2023-05-24 15:54:21 +0900

    fix: CarrierService
    CarrierService

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,8 +3,11 @@ import {LATEST_API_VERSION, shopifyApi} from "@shopify/shopify-api";
 
 const router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
-    res.send('API is working!');
+// shopify 의 callbak url 은 post 로만 가능하다.
+router.post('/', (req: Request, res: Response) => {
+    const jsonData =
+        { "rates": [ { "service_name": "canadapost-overnight", "service_code": "ON", "total_price": "1295", "description": "This is the fastest option by far", "currency": "CAD", "min_delivery_date": "2013-04-12 14:48:45 -0400", "max_delivery_date": "2024-04-12 14:48:45 -0400" }, { "service_name": "fedex-2dayground", "service_code": "2D", "total_price": "2934", "currency": "USD", "min_delivery_date": "2013-04-12 14:48:45 -0400", "max_delivery_date": "2024-04-12 14:48:45 -0400" }, { "service_name": "fedex-priorityovernight", "service_code": "1D", "total_price": "3587", "currency": "USD", "min_delivery_date": "2013-04-12 14:48:45 -0400", "max_delivery_date": "2024-04-12 14:48:45 -0400" } ] }
+    res.send(jsonData);
 });
 
 router.get('/users', (req: Request, res: Response) => {
